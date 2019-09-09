9aabf4c8cc61d2fd09e7a68e661836c403374caf
Joel Parkinson <joelparkinson@live.co.uk>
2019-09-09 05:46:19 +0100

    Extend TodoItem entity
    Allows for the newing up of a TodoItem more cleanly.

--- src/Todo.BlazorUi/Entities/TodoItem.cs
+++ src/Todo.BlazorUi/Entities/TodoItem.cs
@@ -9,5 +9,25 @@ namespace Todo.BlazorUi.Entities
         public string Name { get; set; }
         public string Description { get; set; }
         public DateTime? DueDate { get; set; }
+
+        public TodoItem() { }
+
+        public TodoItem(string description)
+        {
+            Description = description;
+        }
+
+        public TodoItem(string description, string name)
+        {
+            Description = description;
+            Name = name;
+        }
+
+        public TodoItem(string description, string name, DateTime? dueDate)
+        {
+            Description = description;
+            Name = name;
+            DueDate = dueDate;
+        }
     }
 }
