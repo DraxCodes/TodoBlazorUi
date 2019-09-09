1d43c041dce4fd08a9ad38f3843be0b5991b30c2
Joel Parkinson <joelparkinson@live.co.uk>
2019-09-09 04:01:57 +0100

    Start of code refactor.

---
+++ src/Todo.BlazorUi/Handlers/TodoApiHandler.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Todo.BlazorUi.Handlers
+{
+    public class TodoApiHandler
+    {
+        public string TaskId { get; set; } = string.Empty;
+
+        public string TodoDescription { get; set; } = string.Empty;
+
+
+
+    }
+}
