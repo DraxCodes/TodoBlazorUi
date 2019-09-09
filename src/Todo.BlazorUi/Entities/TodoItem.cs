3c4a0af091a565f2511c5183f990e5d55d9197ee
Joel Parkinson <joelparkinson@live.co.uk>
2019-09-09 04:00:37 +0100

    Minor Refactor and Entity extension

---
+++ src/Todo.BlazorUi/Entities/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Todo.BlazorUi.Entities
+{
+    public class TodoItem
+    {
+        public Guid Id { get; set; }
+        public bool IsCompleted { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime? DueDate { get; set; }
+    }
+}
