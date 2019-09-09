604741a3026a63453d434f87891bab76ee0517f4
Joel Parkinson <joelparkinson@live.co.uk>
2019-09-09 05:49:32 +0100

    Create static LoginDetails class
    Allows me to store login details in memory.
    This allows the users details to persist through page changes.
    
    The user still has to login again but the details are filled out for them.
    User has to login again so that they're able to change login info and API urls if needed, without restarting the whole Ui.

---
+++ src/Todo.BlazorUi/PersistantData/LoginDetails.cs
@@ -0,0 +1,10 @@
+﻿namespace Todo.BlazorUi.PersistantData
+{
+    public static class LoginDetails
+    {
+        public static string Username { get; set; }
+        public static string Password { get; set; }
+        public static string ApiUrl { get; set; }
+        public static bool AlreadyHasAuth { get; set; }
+    }
+}
